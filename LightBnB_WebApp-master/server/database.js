24ccb5cf8c3c5aebda5352516f7c0bc4f0947286
Lauren Ashley <la.zaubi@gmail.com>
2022-12-07 13:40:51 -0800

    fix my reservations query to include rows from properties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -78,13 +78,14 @@ exports.addUser = addUser;
  */
 const getAllReservations = function(guest_id, limit = 10) {
   return pool
-    .query(`SELECT *
+    .query(`SELECT reservations.*, properties.*
       FROM reservations
+      JOIN properties ON properties.id = reservations.property_id
       WHERE guest_id = $1
       LIMIT $2;`,
       [guest_id, limit])
     .then((result) => {
-      return result.rows[0];
+      return result.rows;
     })
     .catch((err) => {
       console.log(err.message);
